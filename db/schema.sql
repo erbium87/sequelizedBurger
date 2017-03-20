defcf9dda7d6b63a5d694c976b891d9eb753b810
Meredith Erb <mereditherb@merediths-mbp.attlocal.net>
2017-03-19 22:01:16 -0400

    cleaned up some commented out code

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,3 @@
 CREATE DATABASE moreBurger_db;
 USE moreBurger_db;
 
--- CREATE TABLE burgers
--- (
--- `id` INT AUTO_INCREMENT NOT NULL,
--- `burger_name` varchar(50) NOT NULL,
--- `devoured` BOOLEAN DEFAULT FALSE,
--- `date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
--- PRIMARY KEY (`id`)
--- );
--- models/burger.js
+++ models/burger.js
@@ -15,26 +15,5 @@ module.exports = function(sequelize, DataTypes) {
   return Burgers;
 }
 
-// var orm = require("../config/orm.js");
-
-// var burger = {
-//   all: function(cb) {
-//     orm.all("burgers", function(res) {
-//       cb(res);
-//     });
-//   },
-//   insertOne: function(cols, vals, cb) {
-//     orm.insertOne("burgers", cols, vals, function(res) {
-//       cb(res);
-//     });
-//   },
-//   update: function(objColVals, condition, cb) {
-//     orm.update("burgers", objColVals, condition, function(res) {
-//       cb(res);
-//     });
-//   }
-// };
-
-// module.exports = burger;
 
 
